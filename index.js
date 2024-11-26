7d68c5a97477d95a91611647fbbc8226af1fa6bc
Victor Karakawa <victorhtkarakawa@gmail.com>
2024-11-25 21:50:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ let produtos = [];
 
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    res.redirect('/login');
+  });
+  
+
 app.get('/login', (req, res) => {
   res.render('login');
 });
